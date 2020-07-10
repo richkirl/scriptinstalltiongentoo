6350d523e83f93598c2de5f77d5fc6f4927af490
richkirl <61930048+richkirl@users.noreply.github.com>
2020-07-10 15:29:33 +0300

    Add files via upload

---
+++ gentoinst.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+
+fsroot='/dev/sda6'
+
+fsboot='/dev/sda4'
+
+mntgentoo='/mnt/gentoo'
+
+mntswapgent='dev/sda5'
+
+mntbootgentoo='/mnt/gentoo/boot/EFI'
+
+
+mkfs.fat -F32 $fsboot
+
+mkfs.ext4 $fsroot
+
+mkswap $mntswapgent
+
+swapon $mntswapgent
+
+
+mkdir $mntgentoo
+
+mount $fsroot $mntgentoo
+
+mkdir -p $mntbootgentoo
+
+mount $fsroot $mntbootgentoo
+
+cd $mntgentoo
+
+echo copy next link paste to browser and select stage3 tar and copy adress for stage3 and paste link to stage3 in console for download stage3*tar.xz
+
+read download
+
+wget -4 $download
+
+tar xJvpf stage3-*.tar.xz
+
+echo 'USE="python"' >> /mnt/gentoo/etc/portage/make.conf
+
+echo 'MAKEOPTS="-j13"' >> /mnt/gentoo/etc/portage/make.conf
+
+echo 'LINGUAS="ru"' >> /mnt/gentoo/etc/portage/make.conf
+
+echo 'L10N="ru"' >> /mnt/gentoo/etc/portage/make.conf
+
+echo 'VIDEO_CARDS="intel i915 nouveau"' >> /mnt/gentoo/etc/portage/make.conf
+
+echo 'INPUT_DEVICES="libinput synaptics keyboard mouse evdev"' >> /mnt/gentoo/etc/portage/make.conf
+
+echo 'EMERGE_DEFAULT_OPTS="${EMERGE_DEFAULT_OPTS} --quiet-build=y"' >> /mnt/gentoo/etc/portage/make.conf
+
+mkdir -p /mnt/gentoo/etc/portage/repos.conf
+
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+
+cp -L /etc/resolv.conf /mnt/gentoo/etc/
+
+mount -t proc /proc $mntgentoo/proc
+mount --rbind /dev $mntgentoo/dev
+mount --rbind /sys $mntgentoo/sys
+
+chroot $mntgentoo /bin/bash
